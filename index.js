a698a90c9eef565be8232cdf6e1a18bd4d883242
junxian428 <wengyin777@gmail.com>
2023-09-07 10:24:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+// Create a new SQLite database or connect to an existing one
+const db = new sqlite3.Database('./db.sqlite');
+
+// Create a table for users (if not exists)
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+  )
+`);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  const data = {
+    pageTitle: 'Login Page',
+    message: 'Login Page',
+    errormessage: '',
+  };
+  res.render('index', data);
+});
+
+
+// Assuming you have an array of questions stored in `questions`
+const questions = [
+  'Question 1: What is ...?',
+  'Question 2: Who was ...?',
+  'Question 3: What is ...?',
+  'Question 4: Who was ...?',
+  // Add all your questions here
+];
+
+let currentQuestionIndex = 0;
+
+
+app.get('/quiz', (req, res) => {
+  const username = req.query.username; // Extracting username from the query parameter
+
+  const data = {
+    pageTitle: 'Quiz',
+    message: `Quiz Answer for ${username}`, // Using the username in the message
+    errormessage: '',
+  };
+
+  if (currentQuestionIndex < questions.length) {
+    const questionData = {
+      pageTitle: 'Quiz',
+      question: questions[currentQuestionIndex],
+      questionNumber: currentQuestionIndex + 1,
+      username: username, // Adding username to data for rendering
+    };
+    res.render('quiz', questionData);
+    
+    currentQuestionIndex++;
+  } else {
+    currentQuestionIndex = 0;
+      res.send(`
+      <body style="       background-color: #222;color: #fff;font-family: Arial, sans-serif;">
+      <p style="text-align:center; margin-top:100px;">Quiz completed for ${username}!</p>
+      <br>
+      <br>
+          <form action="/welcome/${username}" method="get" style="    text-align: center;">
+          <input type="hidden" name="username" value="${username}">
+          <button type="submit" style="width: 100%; box-sizing: border-box; padding: 10px; background-color: #00ff00; color: #fff;"style="width: 100%; box-sizing: border-box; padding: 10px; background-color: #00ff00; color: #fff;">Go to Dashboard</button>
+        </form>
+      </body>
+   
+    `);
+    }
+});
+
+app.post('/quiz', (req, res) => {
+  const userAnswer = req.body.answer; // Assuming 'answer' is the name attribute of your radio buttons
+  // Do something with the user's answer (e.g., compare with the correct answer)
+
+  // Render the next question
+  res.redirect(`/quiz?username=${req.body.username}`);
+});
+
+app.get('/welcome/:username', (req, res) => {
+  const username = req.params.username;
+ 
+  const data = {
+    pageTitle: 'Welcome Page',
+    message: `Welcome, ${username}!`,
+    username: username,
+  };
+  res.render('welcome', data); // Assuming you have a 'welcome.ejs' file in your 'views' folder
+  
+});
+
+
+app.get('/change-username', (req, res) => {
+  const username = req.body.username;
+  res.render('changeUsername', { username: username});
+});
+
+
+app.post('/change-username', (req, res) => {
+  const newUsername = req.body.newUsername;
+  const username = newUsername;
+
+  // Assuming you have a function to update the username in the database
+  // Update the username in your database here
+
+  res.send(`
+  <body style="       background-color: #222;color: #fff;font-family: Arial, sans-serif;">
+
+  <p style="text-align:center; margin-top:100px;">Username changed to ${newUsername}</p>
+  <form action="/welcome/${newUsername}" method="get" style="text-align:center;">
+    <input type="hidden" name="username" value="${username}">
+    <button type="submit" style="width: 100%; box-sizing: border-box; padding: 10px; background-color: #00ff00; color: #fff;">Go to Dashboard</button>
+  </form>
+
+  </body>
+`);
+});
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  db.get('SELECT * FROM users WHERE username=? AND password=?', [username, password], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    if (row) {
+      const data = {
+        pageTitle: 'Welcome Page',
+        message: `Welcome, ${row.username}!`,
+        username: username,
+      };
+      res.render('welcome', data); // Assuming you have a 'welcome.ejs' file in your 'views' folder
+    } else {
+      const data = {
+        pageTitle: 'Login Page',
+        errormessage: 'Invalid username or password.',
+        message: 'Login Page',
+      };
+      res.render('index', data);
+    }
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
